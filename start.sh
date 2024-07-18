d21809348dc53391124b1ac34e92c5bb11403b33
Slisk Lindqvist <slisk.lindqvist@querzion.com>
2024-07-18 04:34:38 +0200

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+############ COLOURED BASH TEXT
+RED='\033[0;31m'
+YELLOW='\033[0;33m'
+GREEN='\033[0;32m'
+CYAN='\033[0;36m'
+PURPLE='\033[0;35m'
+BLUE='\033[0;34m'
+NC='\033[0m' # No Color
